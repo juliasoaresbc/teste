94a22878b75ee99ca88caa8fc76469155b2cc234
Julia Soares Barreto Costa <0000110676187xsp@al.educacao.sp.gov.br>
2024-08-14 11:49:07 -0300

    first commit

---
+++ variaveis.py
@@ -0,0 +1,22 @@
+media = 0 
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = 'Júlia', 17                       
+estado = True 
+
+# # função type () --> serve para mostrar qual é o tipo de variável
+# print(type(media))
+# print(type(n2))
+# print(type(nome))
+# print(type(estado))
+# print(type(1+2j)) #valor "complexo"
+
+#função isinstance()
+# a = 10
+# b = 'sol'
+# print(isinstance(b, str))
+# print (isinstance9a,(int,float)))
+
+a = 40
+c = 3
+r = a * c 
+print(r)
